a3959b55767ebd97ba31c66c30226d228890d518
jrocvideos <jrocvideos@gmail.com>
2026-05-05 15:42:18 -0700

    feat: CTO dashboard at /cto

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import { DriverDownload } from './pages/DriverDownload';
 import { YolandaDashboard } from './pages/YolandaDashboard';
 import { PeterDashboard } from './pages/PeterDashboard';
 import { CTODashboard } from './pages/CTODashboard';
+import { CTODashboard } from './pages/CTODashboard';
 
 function AppInner() {
   const cart = useCart();
@@ -53,6 +54,7 @@ function AppInner() {
           <Route path="/restaurant-dashboard" element={<RestaurantDashboard />} />
           <Route path="/biz" element={<YolandaDashboard />} />
           <Route path="/cgo" element={<PeterDashboard />} />
+          <Route path="/cto" element={<CTODashboard />} />
           <Route path="/cto" element={<CTODashboard />} />n          <Route path="/driver-download" element={<DriverDownload />} />
           
               </Routes>
